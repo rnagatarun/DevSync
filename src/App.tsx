4cd06d6c27c2df6cd6cc26606907372dbbd4c9ae
NagaTarun <rnagatarun@gmail.com>
2025-06-08 23:54:39 +0530

    add Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import Navbar from "./components/Navbar"
  
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">DevSync</h1>
+      <Navbar />
     </>
   )
 }
