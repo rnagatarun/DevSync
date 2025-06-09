262ce4b3aaa997b6538ae3bd0a9ba299ed9415a4
NagaTarun <rnagatarun@gmail.com>
2025-06-09 18:17:03 +0530

    add login and authentication

---
+++ src/components/Login.tsx
@@ -0,0 +1,104 @@
+import axios from "axios";
+import { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post("http://localhost:5173/login", {
+        email,
+        password,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <div className="flex justify-around my-[10%]">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body flex flex-col gap-6">
+          <h2 className="card-title">Login</h2>
+          <div className="Email">
+            <label className="input validator">
+              <svg
+                className="h-[1em] opacity-50"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                </g>
+              </svg>
+              <input
+                type="email"
+                placeholder="mail@site.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </label>
+            <div className="validator-hint hidden">
+              Enter valid email address
+            </div>
+          </div>
+
+          <div className="Password">
+            <label className="input validator">
+              <svg
+                className="h-[1em] opacity-50"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                  <circle
+                    cx="16.5"
+                    cy="7.5"
+                    r=".5"
+                    fill="currentColor"
+                  ></circle>
+                </g>
+              </svg>
+              <input
+                type="password"
+                required
+                placeholder="Password"
+                minLength={8}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+              />
+            </label>
+            <p className="validator-hint hidden">
+              Must be more than 8 characters, including
+              <br />
+              At least one number <br />
+              At least one lowercase letter <br />
+              At least one uppercase letter
+            </p>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLogin}>Sign In</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
