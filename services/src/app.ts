2683290332547449a6d681e2022b4b3b496a18a5
NagaTarun <rnagatarun@gmail.com>
2025-06-12 00:21:24 +0530

    establish database connection on express server

--- services/src/app.ts
+++ services/src/app.ts
@@ -1,12 +1,23 @@
 import express from 'express';
+import connectDB from './database.js';
 
 const app = express();
 const port = 3000;
 
-app.use('/', (req, res) => {
-  res.send('Hello World!');
-});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+
+
+// Connect to the database
+connectDB().then(() => {
+  console.log('Database connection established');
+
+  app.use('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}).catch((error) => {
+  console.error('Failed to connect to the database:', error);
 });
