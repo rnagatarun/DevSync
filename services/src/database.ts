c0bbb6f62d256727d6d0707d987c545203708295
NagaTarun <rnagatarun@gmail.com>
2025-06-12 00:20:51 +0530

    create a database connection using mongoose

---
+++ services/src/database.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const dbURI = `mongodb+srv://${process.env.VITE_MONGO_USERNAME}:${process.env.VITE_MONGO_PASSWORD}@devsync.kjowknq.mongodb.net/?retryWrites=true&w=majority`;
+    await mongoose.connect(dbURI, {});
+    console.log('Database connected successfully');
+  } catch (error) {
+    console.error('Database connection error:', error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
