9f232b4f1abec9a6cc200a5f0408d12d3b57e3d7
NagaTarun <rnagatarun@gmail.com>
2025-06-12 23:24:27 +0530

    sanitise at schema level by adding various validations

--- services/src/models/user.ts
+++ services/src/models/user.ts
@@ -1,24 +1,72 @@
 import mongoose from 'mongoose';
+import validator from 'validator';
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 50,
     },
     lastName: {
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true,
+        trim: true,
+        validate(value: string) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Invalid email address: " + value)
+            }
+
+        }
     },
     password: {
-        type: String
+        type: String,
+        required: true,
+        validate(value: string) {
+            if (!validator.isStrongPassword(value)) {
+                throw new Error("Enter a strong Password: " + value)
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18
     },
     gender: {
-        type: String
+        type: String,
+        required: true,
+        validate(value: string) {
+            if (!["Male", "Female", "Others"].includes(value)) {
+                throw new Error("Gender is not valid");
+            }
+        }
+
     },
-});
+    photoUrl: {
+        type: String,
+        default: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+        validate(value: string) {
+            if (!validator.isURL(value)) {
+                throw new Error("Invalid Photo URL: " + value)
+            }
+        }
+    },
+    about: {
+        type: String,
+        default: "This is a default about of the user!",
+    },
+    skills: {
+        type: [String],
+    },
+},
+    {
+        timestamps: true
+    }
+);
 
 export const User = mongoose.model("User", userSchema);
