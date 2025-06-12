be208de5e3d6bd336e9cc2d79cb18d80cd3c240e
NagaTarun <rnagatarun@gmail.com>
2025-06-12 18:59:32 +0530

    add API's to perform CRUD operations on data

--- services/src/app.ts
+++ services/src/app.ts
@@ -7,6 +7,7 @@ const port = 3000;
 //Add middleware to handle data and make it json to javascript object
 app.use(express.json());
 
+//SignUp Api
 app.post('/signup', async (req, res) => {
   const user = new User(req.body);
 
@@ -19,6 +20,76 @@ try {
   }
 });
 
+//getUserByEmail Api
+app.get('/user', async(req,res) => {
+  const userEmail = req.body.emailId;
+  try{
+    const users = await User.find({emailId: userEmail});
+    if((users).length === 0){
+      res.status(400).send("User not found");
+    }
+    else{
+      res.send(users);
+    }
+  }
+  catch{
+      res.status(400).send("Something went wrong");
+  }
+});
+
+//Feed Api
+app.get('/feed', async(req, res) => {
+  try{
+    const users = await User.find({});
+    res.send(users);
+  }
+  catch{
+    res.status(404).send("Something went wrong")
+  }
+});
+
+//Delete User Api
+app.delete('/user', async(req,res) => {
+  const userId = req.body.userId;
+  try{
+    await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  }
+  catch{
+    res.status(400).send("Something went wrong");
+  }
+
+})
+
+//Update User Api
+app.patch('/user', async(req,res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try{
+    await User.findByIdAndUpdate({_id:userId}, data);
+    res.send("User updated successfully");
+  }
+  catch{
+    res.status(400).send("Something went wrong");
+  }
+})
+
+// Update User by Email ID API
+app.patch('/updateByEmail', async (req, res) => {
+  const emailId = req.body.emailId;
+  const data = req.body;
+
+  try {
+    const updatedUser = await User.findOneAndUpdate({ emailId: emailId }, data, { new: true });
+    if (!updatedUser) {
+      res.status(404).send("User not found");
+    } else {
+      res.send("User updated successfully");
+    }
+  } catch {
+    res.status(400).send("Something went wrong");
+  }
+});
 
 // Connect to the database
 connectDB().then(() => {
