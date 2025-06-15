144bb863667128853e72ff5974b348bf4920bf5f
NagaTarun <rnagatarun@gmail.com>
2025-06-15 19:28:16 +0530

    add helper function to validate allowedFields to modify in editProfile

--- services/src/utils/validation.ts
+++ services/src/utils/validation.ts
@@ -7,15 +7,44 @@ interface SignUpRequestBody {
     password: string;
 }
 
+interface EditProfileRequestBody {
+    firstName?: string;
+    lastName?: string;
+    photoUrl?: string;
+    gender?: string;
+    age?: number;
+    about?: string;
+    skills?: string[];
+}
+
+interface EditProfileRequest {
+    body: EditProfileRequestBody;
+}
+
 export const validateSignUpData = (req: { body: SignUpRequestBody }) => {
-    const {firstName, lastName, emailId, password} = req.body;
-    if(!firstName || !lastName){
+    const { firstName, lastName, emailId, password } = req.body;
+    if (!firstName || !lastName) {
         throw new Error("Name is not valid");
     }
-    else if(!validator.isEmail(emailId)){
+    else if (!validator.isEmail(emailId)) {
         throw new Error("Invalid email format");
     }
-    else if(!validator.isStrongPassword(password)){
+    else if (!validator.isStrongPassword(password)) {
         throw new Error("Password must contain at least 8 characters, including uppercase, lowercase, numbers, and special characters");
     }
+}
+
+export const allowedEditFields = [
+    "firstName",
+    "lastName",
+    "photoUrl",
+    "gender",
+    "age",
+    "about",
+    "skills",
+];
+
+export const validateEditProfileData = (req: EditProfileRequest): boolean => {
+    const isEditAllowed = Object.keys(req.body).every((field) => allowedEditFields.includes(field));
+    return isEditAllowed;
 }
