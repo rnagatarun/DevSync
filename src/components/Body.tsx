8a0eefe69ecb62167649b311de23ab31c86d414d
NagaTarun <rnagatarun@gmail.com>
2025-06-21 00:21:08 +0530

    call useProfile hooknin body

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
+import { useProfile } from "../hooks/useProfile";
 
 const Body = () => {
+  useProfile();
   return (
     <div>
       <Navbar />
