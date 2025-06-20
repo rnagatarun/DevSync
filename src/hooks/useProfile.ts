86656e87176056d19b9d796f1f562d5f6c01806b
NagaTarun <rnagatarun@gmail.com>
2025-06-21 00:20:41 +0530

    add useProfile hook to store profile in store

---
+++ src/hooks/useProfile.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../store/userSlice";
+import { useEffect } from "react";
+import type { RootState } from "../store/store";
+
+export const useProfile = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const userData = useSelector((store: RootState) => store.user)
+
+    const fetchData = async () => {
+        if (userData._id) {
+            return;
+        }
+        try {
+            const response = await axios.get(BASE_URL + "/profile/view", {
+                withCredentials: true
+            });
+            dispatch(addUser(response.data.data))
+        }
+        catch (error) {
+            if (axios.isAxiosError(error) && error.response?.status === 401) {
+                navigate("/login")
+            }
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    },[])
+
+    // Redirect to login if unauthenticated
+    useEffect(() => {
+        if (!userData._id) {
+            navigate("/login");
+        }
+    }, [userData._id, navigate])
+
+}
