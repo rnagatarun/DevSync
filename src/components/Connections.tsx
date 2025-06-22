6f7c353bc53e58d325cf8dcde2fc37bef4419813
NagaTarun <rnagatarun@gmail.com>
2025-06-22 17:06:48 +0530

    create connections component for presentation layer

---
+++ src/components/Connections.tsx
@@ -0,0 +1,44 @@
+import { useSelector } from "react-redux";
+import useGetConnections from "../hooks/useGetConnections"
+import type { RootState } from "../store/store";
+
+const Connections = () => {
+  const connections = useSelector((store: RootState) => store.connections) ?? []
+  useGetConnections();
+
+  if(!connections) return;
+
+  if(connections.length === 0) return <h1>No Connections Found</h1>;
+
+  return (
+    <div className=" text-center my-10">
+      <h1 className="text-2xl font-bold">Connections</h1>
+      {connections.map((connection) => {
+        const { _id, firstName, lastName, photoUrl, age, gender, about } = connection;
+        return (
+         <div
+            key={_id}
+            className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto"
+          >
+            <div>
+              <img
+                alt="photo"
+                className="w-20 h-20 rounded-full object-cover"
+                src={photoUrl}
+              />
+            </div>
+            <div className="text-left mx-4 ">
+              <h2 className="font-bold text-xl">
+                {firstName + " " + lastName}
+              </h2>
+              {age && gender && <p>{age + ", " + gender}</p>}
+              <p>{about}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  )
+}
+
+export default Connections
