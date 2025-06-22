eb11cce8d611346b700a00999a82584b0ce50bc0
NagaTarun <rnagatarun@gmail.com>
2025-06-22 15:48:31 +0530

    add edit profile component and display the live editing

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,9 +1,15 @@
+import type { RootState } from "../store/store";
+import type { UserProps } from "../utils/interface";
+import EditProfile from "./EditProfile";
+import { useSelector } from "react-redux";
+
 const Profile = () => {
+  const user = useSelector((state: RootState) => state.user) as UserProps;
   return (
     <div>
-        Profile      
+      <EditProfile user={user} />
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
