2c99997dd0d0e8b1443ccda762c7c4e0dc59151c
NagaTarun <rnagatarun@gmail.com>
2025-06-22 17:05:00 +0530

    create connections Slice and add reducer to appStore

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice.js';
 import feedReducer from './feedSlice.js';
+import ConnectionsReducer from './connectionsSlice.js';
 
 const appStore = configureStore({
     reducer: {
         user: userReducer,
         feed: feedReducer,
+        connections: ConnectionsReducer,
     }
 });
 
