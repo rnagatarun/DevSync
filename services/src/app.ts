aa3924107a2395ce4ed50e09bc75e98791c1610f
NagaTarun <rnagatarun@gmail.com>
2025-06-24 23:42:26 +0530

    remove api from /

--- services/src/app.ts
+++ services/src/app.ts
@@ -23,10 +23,10 @@ app.use(cors({
 );
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api", authRouter);
-app.use("/api", profileRouter);
-app.use("/api", requestRouter);
-app.use("/api", userRouter);
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
+app.use("/", userRouter);
 
 
 //getUserByEmail Api
